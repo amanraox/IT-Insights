aec50ebab26887952faa1ee5707559d91fc61659
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-21 01:39:21 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyDUrhFN-LYbTrGbm7WWLHooWgS7uyN8G_Q",
+  authDomain: "blogss-1d0b8.firebaseapp.com",
+  projectId: "blogss-1d0b8",
+  storageBucket: "blogss-1d0b8.appspot.com",
+  messagingSenderId: "366046660362",
+  appId: "1:366046660362:web:16b729e0e86809e743dcf3",
+  measurementId: "G-VMVCFY393W"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
